a9e61da91bd6b2e22d2a5738903c11a9414a9095
eduarchilon <eduarchilon@outlook.es>
2023-11-19 02:39:25 -0300

    fix qr in page

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
-import "dotenv/config"
-import express from "express"
-import cors from "cors"
-import routes from "./infrastructure/router"
-const port = process.env.PORT || 3001
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static('tmp'))
-app.use(`/`,routes)
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import routes from "./infrastructure/router";
+import WsTransporter from "./infrastructure/repositories/ws.external";
+const port = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("tmp"));
 
-app.listen(port, () => console.log(`Ready...${port}`))
+const transporter = new WsTransporter();
+app.get("/init", (req, res) => {
+  const htmlContent = transporter.getReturnHtml();
+  res.send(htmlContent);
+});
+app.use(`/`, routes);
+
+app.listen(port, () => console.log(`Ready...${port}`));
--- src/infrastructure/repositories/ws.external.ts
+++ src/infrastructure/repositories/ws.external.ts
@@ -2,7 +2,8 @@ import { Client, LocalAuth } from "whatsapp-web.js";
 import { image as imageQr } from "qr-image";
 import LeadExternal from "../../domain/lead-external.repository";
 const fs = require("fs");
-
+var QRCode = require("qrcode");
+var QRTerminal = require("qrcode-terminal");
 /**
  * Extendemos los super poderes de whatsapp-web
  */
@@ -63,18 +64,31 @@ class WsTransporter extends Client implements LeadExternal {
     return this.status;
   }
 
-  private generateImage = (base64: string) => {
+  private html: string = "";
+  getReturnHtml(): string {
+    return this.html;
+  }
+
+  public generateImage = (base64: string) => {
     const path = `${process.cwd()}/tmp`;
     let qr_svg = imageQr(base64, { type: "svg", margin: 4 });
     qr_svg.pipe(require("fs").createWriteStream(`${path}/qr.svg`));
     console.log(`⚡ Recuerda que el QR se actualiza cada minusto ⚡'`);
     console.log(`⚡ Actualiza F5 el navegador para mantener el mejor QR⚡`);
+    console.log(`https://whats-app-api-production.up.railway.app/init`);
     const svgContent = fs.readFileSync(`${path}/qr.svg`, "utf-8");
-    console.log("Contenido del archivo SVG:");
-    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++");
-    console.log(" ");
-    console.log(svgContent);
-    console.log(" ");
+    this.html = `<div style="width: 400px">${svgContent}</div>`;
+    // console.log("Contenido del archivo SVG:");
+    // console.log("+++++++++++++++++++++++++++++++++++++++++++++++++");
+    // console.log(" ");
+    // console.log(" ");
+    // QRCode.toDataURL(base64)
+    //   .then((url: any) => {
+    //     // QRTerminal.generate(url);
+    //   })
+    //   .catch((err: any) => {
+    //     console.error(err);
+    //   });
   };
 }
 
